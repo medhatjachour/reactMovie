9f60854d4c97d6f32e941fd6d326ab2b80d9a6c4
medhatjachour <53466991+medhatjachour@users.noreply.github.com>
2024-07-23 23:01:01 +0300

    init

--- src/components/elements/Header/Header.js
+++ src/components/elements/Header/Header.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
+import logo from '../../../img/tmdb_logo.png'
 
 const Header = () => (
   <div className="rmdb-header">
     <div className="rmdb-header-content">
       <Link to="/">
-        <img className="rmdb-logo" src="/images/reactMovie_logo.png" alt="rmdb-logo" />
+        <img className="rmdb-logo" src={"/images/reactMovie_logo.png"} alt="rmdb-logo" />
       </Link>
-      <img className="rmdb-tmdb-logo" src="/images/tmdb_logo.png" alt="tmdb-logo" />
+      <img className="rmdb-tmdb-logo" src={logo} alt="tmdb-logo" />
     </div>
   </div>
 )
